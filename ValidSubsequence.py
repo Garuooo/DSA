3bc71a52048de34f4bc8e31b1bde60bfc4bc3f4f
Garuooo <83463726+Garuooo@users.noreply.github.com>
2022-11-19 17:08:42 +0200

    Add files via upload

---
+++ ValidSubsequence.py
@@ -0,0 +1,24 @@
+def isValidSubsequence(array, sequence):
+    sequence_index = 0
+    array_index=0
+    while(array_index<len(array) and sequence_index < len(sequence)):
+        if array[array_index]==sequence[sequence_index]:
+            array_index+=1
+            sequence_index+=1
+        else:
+            array_index+=1
+    if sequence_index==len(sequence):
+        return True
+    else:
+        return False
+
+def isValidSubsequence(array, sequence):
+    pointer=0
+    for element in array:
+        if element == sequence[pointer]:
+            pointer+=1
+        if pointer==len(sequence):
+            return True
+    return False
+    
+print(isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10],[1, 6, -1, 10]))
